6673669112174e304d0b1caa009bb254f1abcb7c
62ramire76topcb-design <62ramire76topcb@gmail.com>
2026-09-02 21:15:47 -0400

    Add shared Firebase config module

---
+++ js/firebase-config.js
@@ -0,0 +1,18 @@
+// js/firebase-config.js
+// Configuración compartida de Firebase para todo el proyecto
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEj22gE5UySwWVwIec6jpBiZ2nb1OBftM",
+  authDomain: "agromaxgt-app.firebaseapp.com",
+  projectId: "agromaxgt-app",
+  storageBucket: "agromaxgt-app.firebasestorage.app",
+  messagingSenderId: "648144313047",
+  appId: "1:648144313047:web:118a67464d99f2d0462e92"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+const auth = firebase.auth ? firebase.auth() : null;
